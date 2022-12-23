efdb06561287ee401401cd87de3390b41fbe0226
khadijanadir33 <khadijanadir560@gmail.com>
2022-12-23 16:01:02 +0100

    hhh

---
+++ first_server/app.js
@@ -0,0 +1,35 @@
+const http=require('http')
+let server=http.createServer(function(req,res){
+   res.end("hello") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+server.listen(8081)
---
+++ notion de base/firstnode.js
@@ -0,0 +1,30 @@
+// const http=require('http')
+// const fs=require("fs")
+//    const data= JSON.parse(fs.readFileSync("./data.json","utf-8"))
+//  console.log(data)
+ /********************* extraire dans un fichier txt *************************************************/
+//  let newobj={nom:"sara",prenom:"kenza",age:18}
+//  let jsondata=JSON.stringify(newobj)
+//  fs.writeFileSync("./data.json",jsondata)
+ /************************** Modification **********************************************************/ 
+
+//  let obj=JSON.stringify(data)
+//  fs.writeFileSync("./data.json",obj)
+// console.log(data.nom + " " +data.prenom+" "+data.age)
+/**************************************** Ajout ******************************************************/
+// fs.appendFileSync("data.json",','+jsondata+']')
+/*********************** Create File *****************************************************************/
+// let data=[
+//   {
+//   nom:"nadir",
+//   prenom:"khadija",
+//   age:22
+//   }
+// ]
+// let dataTojson=JSON.stringify(data)
+// fs.writeFileSync("./data.json",dataTojson)
+// console.log("successful") 
+/*********************** add in file json **********************************************************/
+
+// data.push({nom:"hhhh",prenom:"hhff",age:11})
+// fs.writeFileSync('data.json',JSON.stringify(data))
